95c3473753001f578e0cb5527d69a88f2f4ddff2
Zicrom <kevin08200@live.fr>
2019-04-05 02:39:28 +0200

    It's working, WIP stability

--- Assets/Scripts/HumanScripts/BodyParts.cs
+++ Assets/Scripts/HumanScripts/BodyParts.cs
@@ -1,41 +1,75 @@
 ﻿using System;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 namespace HumanScripts
 {
 	public class BodyParts : object
 	{
-		private Animator _attachedHuman;
-		private IntPtr _humanRef;
+		private HumanBodyBones _bone;
+		private readonly Animator _attachedHuman;
+		private Int64 _id;
+		private readonly IntPtr _humanRef;
+		
+		public Int64 Id
+		{
+			get { return _id; }
+			set { _id = value; }
+		}
 
-		public Int64 Id { get; set; }
-
-		public HumanBodyBones Bone { get; set; }
+		public HumanBodyBones Bone
+		{
+			get { return _bone; }
+			set { _bone = value; }
+		}
 
 		public BodyParts(Animator attachedHuman, IntPtr humanRef)
 		{
-			if (attachedHuman == null) throw new ArgumentNullException(nameof(attachedHuman));
-			_attachedHuman = attachedHuman;
+            _attachedHuman = attachedHuman ?? throw new ArgumentNullException(nameof(attachedHuman));
 			_humanRef = humanRef;
 		}
 
 		public BodyParts(HumanBodyBones bone, Animator attachedHuman, Int64 id, IntPtr humanRef)
 		{
-			if (attachedHuman == null) throw new ArgumentNullException(nameof(attachedHuman));
-			this.Bone = bone;
-			this._attachedHuman = attachedHuman;
-			this.Id = id;
+            this._bone = bone;
+			this._attachedHuman = attachedHuman ?? throw new ArgumentNullException(nameof(attachedHuman));
+			this._id = id;
 			this._humanRef = humanRef;
 		}
 
 		public void Update()
 		{
-			var position = HumanDLLInterface.getBodyPartPos(this._humanRef, this.Id);
-			var rotation = HumanDLLInterface.getBodyPartQuat(this._humanRef, this.Id);
-			var boneTransform = this._attachedHuman.GetBoneTransform(this.Bone);
+            Debug.Log(this._id);
+            Debug.Log(this._humanRef);
 
-			boneTransform.rotation = new Quaternion((float) rotation[0], (float) rotation[1], (float) rotation[2], (float) rotation[4]);
-			boneTransform.position = new Vector3((float) position[0], (float) position[1], (float) position[2]);
-		}
+            IntPtr position = HumanDLLInterface.getBodyPartPos(this._humanRef, this._id);
+            int arrayLength = 3;
+            Double[] resultPosition = new Double[arrayLength];
+            Marshal.Copy(position, resultPosition, 0, arrayLength);
+
+            Debug.Log("resultPosition 0 "+ resultPosition[0]);
+            Debug.Log("resultPosition 1 " + resultPosition[1]);
+            Debug.Log("resultPosition 2 " + resultPosition[2]);
+            resultPosition[0] = resultPosition[0] / 1000;
+            resultPosition[1] = resultPosition[1] / 1000;
+            resultPosition[2] = resultPosition[2] / 1000;
+
+            IntPtr rotation = HumanDLLInterface.getBodyPartQuat(this._humanRef, this._id);
+            arrayLength = 4;
+            Double[] resultRotation = new Double[arrayLength];
+            Marshal.Copy(rotation, resultRotation, 0, arrayLength);
+
+
+            Debug.Log("resultRotation 0 " + resultRotation[0]);
+            Debug.Log("resultRotation 1 " + resultRotation[1]);
+            Debug.Log("resultRotation 2 " + resultRotation[2]);
+            Debug.Log("resultRotation 3 " + resultRotation[3]);
+
+            var boneTransform = this._attachedHuman.GetBoneTransform(this._bone);
+            Quaternion test = new Quaternion((float)resultRotation[0], (float)resultRotation[1], (float)resultRotation[2], (float)resultRotation[3]);
+            boneTransform.rotation = test;
+            Vector3 test2 = new Vector3((float)resultPosition[0], (float)resultPosition[1], (float)resultPosition[2]);
+            boneTransform.position = test2;
+        }
 	}
 }
--- Assets/Scripts/HumanScripts/HumanScript.cs
+++ Assets/Scripts/HumanScripts/HumanScript.cs
@@ -13,25 +13,26 @@ namespace HumanScripts
 		// Called before everything, used to initialise the object.
 		void Awake()
 		{
-			this._body = new List<BodyParts>();
-			this._humanInstance = HumanDLLInterface.Human_create("/home/postaron/Codes/Unity/TAHBOO/Assets/Scripts/HumanScripts/config.json");
-			if (this._humanInstance == null)
-			{
-				Debug.Log("Error Human instance null");
-			}
-			else
-			{
-				Debug.Log("Human instance create");
-			}
+            this._body = new List<BodyParts>();
+            this._humanInstance = HumanDLLInterface.Human_create("D:\\MASTER\\M1_CHPS\\TER\\TER\\test\\Assets\\Plugin\\config.json");
+            if (this._humanInstance == null)
+            {
+                Debug.Log("Error Human instance null");
 
-			IntPtr tabIds = HumanDLLInterface.getIds(this._humanInstance);
-			int arrayLength = (int) Marshal.ReadInt64(tabIds);
-			IntPtr start = IntPtr.Add(tabIds, sizeof(long));
-			Int64[] result = new Int64[arrayLength];
-			Marshal.Copy(start, result, 0, arrayLength);
+            }
+            else
+            {
+                Debug.Log("Human instance create");
+            }
 
-			Debug.Log("Getid ok");
-			for (long i = 0; i < result.Length; ++i)
+            IntPtr tabIds = HumanDLLInterface.getIds(this._humanInstance);
+            int arrayLength = (int)Marshal.ReadInt64(tabIds);
+            IntPtr start = IntPtr.Add(tabIds, 8);
+            Int64[] result = new Int64[arrayLength];
+            Marshal.Copy(start, result, 0, arrayLength);
+
+            Debug.Log("Getid ok");
+			for (long i = 0; i < result.Length ; ++i)
 			{
 				this._body.Add(new BodyParts(this.GetComponent<Animator>(), this._humanInstance));
 			}
@@ -39,23 +40,24 @@ namespace HumanScripts
 			//Init of all the bones.
 			this._body[0].Bone = HumanBodyBones.Head;
 			this._body[0].Id = result[0];
+            Debug.Log(result[0]);
 
-			this._body[1].Bone = HumanBodyBones.LeftFoot;
+            this._body[1].Bone = HumanBodyBones.LeftFoot;
 			this._body[1].Id = result[1];
-
-			this._body[2].Bone = HumanBodyBones.RightFoot;
+            Debug.Log(result[1]);
+            this._body[2].Bone = HumanBodyBones.RightFoot;
 			this._body[2].Id = result[2];
 
-			this._body[3].Bone = HumanBodyBones.RightUpperLeg;
+            this._body[3].Bone = HumanBodyBones.RightUpperLeg;
 			this._body[3].Id = result[3];
 
-			this._body[4].Bone = HumanBodyBones.LeftHand;
+            this._body[4].Bone = HumanBodyBones.LeftHand;
 			this._body[4].Id = result[4];
 
-			this._body[5].Bone = HumanBodyBones.LeftLowerArm;
+            this._body[5].Bone = HumanBodyBones.LeftLowerArm;
 			this._body[5].Id = result[5];
 
-			this._body[6].Bone = HumanBodyBones.LeftUpperLeg;
+            this._body[6].Bone = HumanBodyBones.LeftUpperLeg;
 			this._body[6].Id = result[6];
 
 			this._body[7].Bone = HumanBodyBones.RightLowerArm;
@@ -77,22 +79,23 @@ namespace HumanScripts
 		// Start is called before the first frame update
 		void Start()
 		{
-		}
+        }
 
 		// Update is called once per frame
 		void Update()
 		{
-			if (HumanDLLInterface.update(this._humanInstance) == 0)
-			{
-				foreach (var parts in this._body)
-				{
-					parts.Update(); //Update des bodyParts : mise à jour dans la scène, « normalement » ça marche.*/
-				}
-			}
-			else
-			{
-				Debug.Log("DTRACK C D LA MERT");
-			}
+            if (HumanDLLInterface.update(this._humanInstance) == 0)
+            {
+                foreach (var parts in this._body)
+                {
+                    /*Debug.Log(parts.Id);*/
+                    parts.Update(); /*Update des bodyParts : mise à jour dans la scène, « normalement » ça marche.*/
+                }
+            }
+            else
+            {
+                Debug.Log("DTRACK C D LA MERT");
+            }
 		}
 
 		void OnApplicationQuit()
